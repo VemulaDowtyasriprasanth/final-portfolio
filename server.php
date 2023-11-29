0578ccead2ffb5fa38281e4f36e5795181725243
VemulaDowtyasriprasanth <www.dowtyasriprasanth1729@gmail.com>
2023-11-29 16:54:28 -0600

    first commit

---
+++ server.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $recipient = "your_email@example.com"; // REPLACE with your email address
+    $subject = "New contact from $name";
+
+    // Email content
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+
+    // Email headers
+    $email_headers = "From: $name <$email>";
+
+    // Send the email
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        // Set a 200 (okay) response code.
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        // Set a 500 (internal server error) response code.
+        http_response_code(500);
+        echo "Oops! Something went wrong, we couldn't send your message.";
+    }
+
+} else {
+    // Not a POST request, set a 403 (forbidden) response code.
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
